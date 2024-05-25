4301755f5ad95f67de70cf63bb3e673bddd7aba6
2040RICARDO <ricardoct20401992@hotmail.com>
2024-05-25 12:33:20 -0400

    asckjshhhsssss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,7 @@
     {{ Html::style('assets/images/favicon.ico') }}
     {{ Html::style('assets/css/vendors/plugins.min.css') }}
     {{ Html::style('assets/css/style.css') }}
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </head>
 <body>
